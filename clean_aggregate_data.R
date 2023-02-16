484eb998620e8a0948596843d06755d0cd99036f
Aaron Gullickson <aarongullickson@gmail.com>
2023-02-16 09:07:15 -0800

    Add beginning of cleaning data script

---
+++ clean_aggregate_data.R
@@ -0,0 +1,31 @@
+# Lets Clean Some Data!
+
+# Load Libraries ----------------------------------------------------------
+
+library(tidyverse)
+
+# Read the data -----------------------------------------------------------
+
+ipums <- read_fwf("data_examples/usa_00084.dat.gz",
+                  col_positions = 
+                    fwf_positions(start = c(1,32,55,74,84,85,88,94),
+                                  end =   c(4,41,56,83,84,87,91,96),
+                                  col_names=c("year","hhwt","statefip","perwt",
+                                              "sex","age","yrmarr","educd")),
+                  progress=TRUE,
+                  col_types = cols(.default = "i"))
+
+# Code year married --------------------------------------------------------
+
+
+# Code gender -------------------------------------------------------------
+
+
+# Education example -------------------------------------------------------
+
+
+# Tidy dataset ------------------------------------------------------------
+
+
+# Aggregate data ----------------------------------------------------------
+
