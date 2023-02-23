ff4d225b359b41983f7938753f1f3fe2c59cca7b
Aaron Gullickson <aarongullickson@gmail.com>
2023-02-23 08:29:56 -0800

    Add start of reshape/merge script

---
+++ merge_reshape_data.R
@@ -0,0 +1,13 @@
+# Aaron Gullickson
+# Soc 512, Winter 2023
+# Reshaping and Merging Data
+
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
+
+
+# Load data ---------------------------------------------------------------
+
+load("output/ipums.RData")
